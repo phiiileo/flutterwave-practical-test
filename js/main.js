bbbcdeb917f59dfc56a274a91a0c35f86377d26c
phiiileo <emmanuelboluwajib1@gmail.com>
2019-12-21 20:12:50 +0100

    changed seckey to mine

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let bvn = document.querySelector("#bvn").value;
 
 console.log(fname, lname, p_num, bvn);
 
-let tokenKey = 'FLWSECK-af1ff421c70907112ba55815f89710ea-X'
+let tokenKey = 'FLWSECK-ac920ae15dd1d0241a19b49fbeea4113-X'
 let request = new XMLHttpRequest();
 let api = "https://ravesandboxapi.flutterwave.com/v2/kyc/bvn/" + bvn + "?seckey=" + tokenKey;
 request.open("GET", api, true);
